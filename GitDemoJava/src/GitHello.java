97312f2763015177a3a1d60fc401958d30cc04de
Deepanshu Gupta <deepanshujindal1021997@gmail.com>
2020-03-07 14:47:50 +0530

    Version 1.1

--- GitDemoJava/src/GitHello.java
+++ GitDemoJava/src/GitHello.java
@@ -3,7 +3,9 @@ public class GitHello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello My My friend Git");
+		System.out.println("Hello My My friend Git");
+		System.out.println("Initial version pushed already");
+		System.out.println("this is changes for 1.1");
 	}
 
 }
